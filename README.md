b49ecc3b93dcd3881f9bedc764863ca27960d415
ERIC DUFFY <dufstar@ERICs-MacBook-Air.local>
2015-10-09 11:59:40 -0400

    move files all around and create all multiple gulp tasks to minify and concat script style and img files into dist folder

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # Pulsey
 Create beautiful pulsey tours for your app
+
+
+Component Map
+1 PulsyTour
+2 PulsyDot      2 PulsyUnderlay
+3 PulsyTooltip
+
+Usage
+- user adds pulsy-tour id div to body of target page
+- user adds pt-anchor tags to all target elements
+
+
+COMPLETED
+- move js styles into separate js file and import into main one?
+- put dot in center of pt-anchor
+- add in click events
+- listen for window resize
+- add states to local storage
+- check to see if pt-anchor has position fixed, and assign that to pulsydot accordingly
+- check to see if pt-anchor has position fixed on scroll
+- fix weird positioning issue
+- easy way to customize tooltip note
+- create underlay when pulsy is active (optional)
+- link closetooltip and close underlay events
+- way for user to modify location of dot (special variable in positioning of the dots?) or can choose to override position altogether
+- create function to render root components
+- send object in through props
+- get ES6 up and running
+
+NEXT
+- convert to ES6
+- automatically run gulp babel and watch for changes
+- install radium
+- add hover effects
+- option to show an opt-out button in the tooltip, which sets localstorage to true for all
+- way to add custom html within tooltip (links, divs with css, etc.) instead of just a string.
+- fire event when all dots have been clicked and set a tourComplete key to true
+
+- option for a 'next' button in all tooltips, which automatically moves you to the next tooltip pulsy
+- automatically toggle a tooltip style that prevents it from being offscreen
+- add a click handler that allows the user to trigger whatever event they want when dot is clicked and when tooltip is closed (likely using component did mount/unmount)
+- restart pulsydots automatically upon hitting the reset button along with clearing localstorage
+- begin the css craziness...in a controlled way
+- choose tooltip pop direction
+- choose whether to have a pointer arrow
+- tooltip transitions
+- dot keyframes and transitions
+- colors for all
+- dot sizes
+
+LATER
+- determine whether pulsy dot is near an edge - default tooltip to left or bottom accordingly
+- make pulsydots track either local or session storage as an option
+- make mobile/touch-screen ready
+- create listener that starts pulsytour based on a user-defined event
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,10 +1,22 @@
-var gulp = require('gulp');
-var webserver = require('gulp-webserver');
-var babel = require("gulp-babel");
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    del = require('del'),
+    webserver = require('gulp-webserver'),
+    babel = require("gulp-babel");
 
 
-gulp.task('webserver', function() {
-  gulp.src('./')
+gulp.task('go', function() {
+  return gulp.src('./')
     .pipe(webserver({
       livereload: true,
       directoryListing: true,
@@ -12,8 +24,47 @@ gulp.task('webserver', function() {
     }));
 });
 
-gulp.task('babel', function() {
-  gulp.src('src/pulsy.js')
+gulp.task('styles', function() {
+  return sass('src/styles/**/*.scss', { style: 'expanded' })
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('src/scripts/**/*.js')
     .pipe(babel())
-    .pipe(gulp.dest('dist'));
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/assets/js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/assets/js'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+gulp.task('images', function() {
+  return gulp.src('src/images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/assets/img'))
+    .pipe(notify({ message: 'Images task complete' }));
+});
+
+gulp.task('clean', function(cb) {
+    del(['dist/assets/css', 'dist/assets/js', 'dist/assets/img'], cb)
+});
+
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts', 'images');
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/styles/**/*.scss', ['styles']);
+  gulp.watch('src/scripts/**/*.js', ['scripts']);
+  gulp.watch('src/images/**/*', ['images']);
+  livereload.listen();
+  gulp.watch(['dist/**']).on('change', livereload.changed);
 });
